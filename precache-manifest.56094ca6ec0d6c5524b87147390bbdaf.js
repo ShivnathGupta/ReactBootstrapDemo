6ece16bfc2ebb751572b46299977d04c351397c3
Shivnath <Shivnath0001@gmail.com>
2020-04-05 19:41:48 +0530

    Updates

---
+++ precache-manifest.56094ca6ec0d6c5524b87147390bbdaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71364958383604fecf67c162dcce584c",
+    "url": "/ReactBootstrapDemo/index.html"
+  },
+  {
+    "revision": "26f634e2c4d3a33200cd",
+    "url": "/ReactBootstrapDemo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a270d4e9ff2ee4adf1ff",
+    "url": "/ReactBootstrapDemo/static/css/main.98045cbd.chunk.css"
+  },
+  {
+    "revision": "26f634e2c4d3a33200cd",
+    "url": "/ReactBootstrapDemo/static/js/2.b0590d7f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/ReactBootstrapDemo/static/js/2.b0590d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a270d4e9ff2ee4adf1ff",
+    "url": "/ReactBootstrapDemo/static/js/main.02fe3489.chunk.js"
+  },
+  {
+    "revision": "15e9306bb47ef59af50d",
+    "url": "/ReactBootstrapDemo/static/js/runtime-main.2df89187.js"
+  }
+]);
